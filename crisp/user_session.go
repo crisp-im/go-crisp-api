3024e66a220a47f437479cdcea9b5e5a5289764c
Valerian Saliou <valerian@valeriansaliou.name>
2016-07-17 16:31:32 +0200

    ✅ User > Account > {Session, Statistics}
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- crisp/user_session.go
+++ crisp/user_session.go
@@ -4,3 +4,72 @@
 // license that can be found in the LICENSE file.
 
 package crisp
+
+
+// UserSessionParametersData mapping
+type UserSessionParametersData struct {
+  Data  *UserSessionParameters  `json:"data"`
+}
+
+// UserSessionParameters mapping
+type UserSessionParameters struct {
+  Identifier  *string  `json:"identifier"`
+  Key         *string  `json:"key"`
+}
+
+// UserSessionLogin mapping
+type UserSessionLogin struct {
+  Email     *string  `json:"email"`
+  Password  *string  `json:"password"`
+}
+
+// UserSessionRecover mapping
+type UserSessionRecover struct {
+  Email  *string  `json:"email"`
+}
+
+
+// CheckSessionValidity checks whether the user is logged in or not, and whether his session is valid or not.
+// Reference: https://docs.crisp.im/api/v1/#user-user-session-head
+func (service *UserService) CheckSessionValidity() (*Response, error) {
+  url := "user/session"
+  req, _ := service.client.NewRequest("HEAD", url, nil)
+
+  return service.client.Do(req, nil)
+}
+
+
+// CreateNewSession logins to user account and create a new session.
+// Reference: https://docs.crisp.im/api/v1/#user-user-session-post
+func (service *UserService) CreateNewSession(email string, password string) (*UserSessionParameters, *Response, error) {
+  url := "user/session/login"
+  req, _ := service.client.NewRequest("POST", url, UserSessionLogin{Email: &email, Password: &password})
+
+  session := new(UserSessionParametersData)
+  resp, err := service.client.Do(req, session)
+  if err != nil {
+    return nil, resp, err
+  }
+
+  return session.Data, resp, err
+}
+
+
+// DestroySession logouts from user account and destroys current session.
+// Reference: https://docs.crisp.im/api/v1/#user-user-session-post-1
+func (service *UserService) DestroySession() (*Response, error) {
+  url := "user/session/logout"
+  req, _ := service.client.NewRequest("POST", url, nil)
+
+  return service.client.Do(req, nil)
+}
+
+
+// RecoverSession recovers an user account from which we are locked out. A password recovery email is sent.
+// Reference: https://docs.crisp.im/api/v1/#user-user-session-post-2
+func (service *UserService) RecoverSession(email string) (*Response, error) {
+  url := "user/session/recover"
+  req, _ := service.client.NewRequest("POST", url, UserSessionRecover{Email: &email})
+
+  return service.client.Do(req, nil)
+}
--- crisp/user_statistics.go
+++ crisp/user_statistics.go
@@ -4,3 +4,31 @@
 // license that can be found in the LICENSE file.
 
 package crisp
+
+
+// UserStatisticsData mapping
+type UserStatisticsData struct {
+  Data  *UserStatistics  `json:"data"`
+}
+
+// UserStatistics mapping
+type UserStatistics struct {
+  UserID  *string  `json:"user_id"`
+  Unread  *int     `json:"unread"`
+}
+
+
+// CountTotalUnreadMessages counts the total number of unread messages, cross-website.
+// Reference: https://docs.crisp.im/api/v1/#user-user-statistics-get
+func (service *UserService) CountTotalUnreadMessages() (*UserStatistics, *Response, error) {
+  url := "user/stats/unread"
+  req, _ := service.client.NewRequest("GET", url, nil)
+
+  stats := new(UserStatisticsData)
+  resp, err := service.client.Do(req, stats)
+  if err != nil {
+    return nil, resp, err
+  }
+
+  return stats.Data, resp, err
+}
