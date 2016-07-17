95c2be65672d6f8c5525568ec223f019954886c1
Valerian Saliou <valerian@valeriansaliou.name>
2016-07-17 22:42:12 +0200

    ✅ Plugins > List
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- crisp/plugins_list.go
+++ crisp/plugins_list.go
@@ -6,6 +6,61 @@
 package crisp
 
 
-// import (
-//   "fmt"
-// )
+import (
+  "fmt"
+  "net/url"
+)
+
+
+// PluginListData mapping
+type PluginListData struct {
+  Data  *[]PluginInformation  `json:"data,omitempty"`
+}
+
+
+// ListAllPlugins lists all available plugins on the marketplace.
+// Reference: https://docs.crisp.im/api/v1/#plugin-plugins-list-get
+func (service *WebsiteService) ListAllPlugins(pageNumber uint) (*[]PluginInformation, *Response, error) {
+  url := fmt.Sprintf("plugins/list/all/%d", pageNumber)
+  req, _ := service.client.NewRequest("GET", url, nil)
+
+  plugins := new(PluginListData)
+  resp, err := service.client.Do(req, plugins)
+  if err != nil {
+    return nil, resp, err
+  }
+
+  return plugins.Data, resp, err
+}
+
+
+// ListFeaturedPlugins lists featured plugins on the marketplace, from the newer to older.
+// Reference: https://docs.crisp.im/api/v1/#plugin-plugins-list-get-1
+func (service *WebsiteService) ListFeaturedPlugins(pageNumber uint) (*[]PluginInformation, *Response, error) {
+  url := fmt.Sprintf("plugins/list/featured/%d", pageNumber)
+  req, _ := service.client.NewRequest("GET", url, nil)
+
+  plugins := new(PluginListData)
+  resp, err := service.client.Do(req, plugins)
+  if err != nil {
+    return nil, resp, err
+  }
+
+  return plugins.Data, resp, err
+}
+
+
+// SearchPlugins searches for plugins in the marketplace, given a search term.
+// Reference: https://docs.crisp.im/api/v1/#plugin-plugins-list-get-2
+func (service *WebsiteService) SearchPlugins(query string, pageNumber uint) (*[]PluginInformation, *Response, error) {
+  url := fmt.Sprintf("plugins/list/search/%d?query=%s", pageNumber, url.QueryEscape(query))
+  req, _ := service.client.NewRequest("GET", url, nil)
+
+  plugins := new(PluginListData)
+  resp, err := service.client.Do(req, plugins)
+  if err != nil {
+    return nil, resp, err
+  }
+
+  return plugins.Data, resp, err
+}
