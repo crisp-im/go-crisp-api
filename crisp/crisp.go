9ebcf745177a8d9f8748892fa21680114aa04af7
Valerian Saliou <valerian@valeriansaliou.name>
2016-10-22 16:36:55 +0300

    v1.15.2
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- crisp/crisp.go
+++ crisp/crisp.go
@@ -18,7 +18,7 @@ import (
 
 
 const (
-  libraryVersion = "1.15.1"
+  libraryVersion = "1.15.2"
   defaultRestEndpointURL = "https://api.crisp.im/v1/"
   defaultRealtimeEndpointURL = "https://app.relay.crisp.im:443/"
   userAgent = "go-crisp-api/" + libraryVersion
