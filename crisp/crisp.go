f4f43392f52bd34fd73f5827d6f2a60e30ae15a1
Valerian Saliou <valerian@valeriansaliou.name>
2024-03-25 11:13:38 +0100

    v3.42.0

--- crisp/crisp.go
+++ crisp/crisp.go
@@ -20,7 +20,7 @@ import (
 
 
 const (
-  libraryVersion = "3.41.1"
+  libraryVersion = "3.42.0"
   defaultRestEndpointURL = "https://api.crisp.chat/v1/"
   userAgent = "go-crisp-api/" + libraryVersion
   acceptContentType = "application/json"
