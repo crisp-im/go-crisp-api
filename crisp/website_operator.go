a6f3ddd4991c2da965c2f2f99d82de51079b0014
Valerian Saliou <valerian@valeriansaliou.name>
2024-10-16 11:39:30 +0200

    Change argument for `Website.InviteWebsiteOperator`

--- crisp/website_operator.go
+++ crisp/website_operator.go
@@ -72,9 +72,9 @@ type WebsiteOperatorEmailTarget struct {
 
 // WebsiteOperatorInvite mapping
 type WebsiteOperatorInvite struct {
-  Email   *string  `json:"email,omitempty"`
-  Role    *string  `json:"role,omitempty"`
-  Verify  *string  `json:"verify,omitempty"`
+  Email  *string  `json:"email,omitempty"`
+  Role   *string  `json:"role,omitempty"`
+  Token  *string  `json:"token,omitempty"`
 }
 
 // WebsiteOperatorEdit mapping
@@ -160,9 +160,9 @@ func (service *WebsiteService) GetWebsiteOperator(websiteID string, userID strin
 
 
 // InviteWebsiteOperator invites an email to join website as operator.
-func (service *WebsiteService) InviteWebsiteOperator(websiteID string, email string, role string, verify string) (*Response, error) {
+func (service *WebsiteService) InviteWebsiteOperator(websiteID string, email string, role string, token string) (*Response, error) {
   url := fmt.Sprintf("website/%s/operator", websiteID)
-  req, _ := service.client.NewRequest("POST", url, WebsiteOperatorInvite{Email: &email, Role: &role, Verify: &verify})
+  req, _ := service.client.NewRequest("POST", url, WebsiteOperatorInvite{Email: &email, Role: &role, Token: &token})
 
   return service.client.Do(req, nil)
 }
